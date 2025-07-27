d4f4ef83cfbb963c7b90c38f1c6267f5791bc9dc
Youps <emaildeyouness@gmail.com>
2025-07-27 15:52:02 +0200

    Welcome to 1.12.1

--- include/config/general.h
+++ include/config/general.h
@@ -67,12 +67,8 @@
 #define GEN_LATEST GEN_9
 
 // General settings
-<<<<<<< HEAD
 #define EXPANSION_INTRO              FALSE    // If TRUE, a custom RHH intro will play after the vanilla copyright screen.
 #define POKEDEX_PLUS_HGSS            TRUE   // If TRUE, enables the custom HGSS style Pokedex.
-=======
-#define EXPANSION_INTRO              TRUE    // If TRUE, a custom RHH intro will play after the vanilla copyright screen.
->>>>>>> efa2fb3be7ba31d2487990259b466d5095a04a9a
 #define SUMMARY_SCREEN_NATURE_COLORS TRUE    // If TRUE, nature-based stat boosts and reductions will be red and blue in the summary screen.
 #define HQ_RANDOM                    TRUE    // If TRUE, replaces the default RNG with an implementation of SFC32 RNG. May break code that relies on RNG.
 #define COMPETITIVE_PARTY_SYNTAX     TRUE    // If TRUE, parties are defined in "competitive syntax".
